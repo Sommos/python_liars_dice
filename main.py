d3536826b2304766c226ff612defed507aaf5aa3
Sommos <Sommosgaming@gmail.com>
2023-08-02 02:10:08 +0100

    added logic for the basic game

--- main.py
+++ main.py
@@ -6,30 +6,66 @@ def roll_dice(num_dice):
 
 def valid_bid(bid, prev_bid):
     # check if the bid is valid and higher than the previous bid
-    pass
+    if bid[0] < prev_bid[0]:
+        return False
+    elif bid[0] == prev_bid[0] and bid[1] <= prev_bid[1]:
+        return False
+    return True
 
 def challenge(prev_bid, players_dice):
     # check if the previous bid is a lie and return True or False
-    pass
+    face_value, quantity = prev_bid
+    total_dice_count = sum(players_dice.values())
+
+    return total_dice_count < quantity or players_dice[face_value] < quantity
 
 def liar_dice_game(num_players, num_dice_per_player, num_rounds):
     # init the game
+    players_dice = {player: num_dice_per_player for player in range(1, num_players + 1)}
 
     # main game loop
-    for round_num in range(i, num_rounds + 1):
+    for round_num in range(1, num_rounds + 1):
         print(f"Round {round_num}")
-
         # round-specific variables
+        current_bid = (0, 0)
+        current_player = random.randint(1, num_players)
 
         while True:
             # player's turn logic
-            pass
+            print(f"Player {current_player}'s turn")
+            print(f"Current bid: {current_bid}")
 
+            # get the player's bid
+            player_input = input("Enter your bid as 'face_value quantity': ")
+            face_value, quantity = map(int, player_input.split())
+
+            bid = (face_value, quantity)
+            
+            if valid_bid(bid, current_bid):
+                # update the current bid
+                current_bid = bid
+                # update the current player
+                current_player = (current_player % num_players) + 1
+            else:
+                print("Invalid bid. Try again.")
+                continue
+            
             # check for a challenge, and end the round if a challenge is made
-            pass
+            challenge_input = input("Challenge? (y/n): ")
+            if challenge_input.lower() == 'y':
+                if challenge(current_bid, players_dice):
+                    # the challenge was successful
+                    print(f"Player {current_player} was lying! They lose a die.")
+                    players_dice[current_player] -= 1
+                else:
+                    # the challenge was unsuccessful
+                    print(f"Player {current_player} was telling the truth! The challenger loses a die.")
+                    players_dice[current_player % num_players + 1] -= 1
+                break
 
     # determine the winner
-    pass
+    winner = max(players_dice, key = players_dice.get)
+    print(f"\nPlayer {winner} wins the game with {players_dice[winner]} dice remaining!")
 
 if __name__ == "__main__":
     # get the number of players, dice per player, and number of rounds from user input
